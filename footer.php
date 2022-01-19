ed7bbd8d25968a684a7be6f756e787b97d98737c
Rocky8787 <98005915+Rocky8787@users.noreply.github.com>
2022-01-19 10:40:45 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+  
+  
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+            <p class="m-0 text-center text-white">BloodBank & Donor Management System by Group 11</p>
+        </div>
+        <!-- /.container -->
+    </footer>
